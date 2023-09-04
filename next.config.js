7d585e7f15f56f2087ac85d2e0978dc4eb1cf24e
Italo Leal <itlcoorporativo@gmail.com>
2023-09-04 17:12:48 -0300

    feat: Setup do projeto

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: [
+    'page.tsx',
+    'api.tsx',
+    'api.tsx',
+  ],
+}
+
+module.exports = nextConfig
