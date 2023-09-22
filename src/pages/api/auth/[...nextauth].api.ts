3c6e8f88adcf166209dee07d4f5591e20364cc89
Italo Leal <itlcoorporativo@gmail.com>
2023-09-22 16:42:30 -0300

    Atualização do perfil: Pagina de atualizacao do perfil

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,11 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse, NextPageContext } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import PrismaAdapter from '@/lib/auth/prisma-adapter'
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
 export function buildNextAuthOption(
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req: NextApiRequest | NextPageContext['req'],
+  res: NextApiResponse | NextPageContext['res'],
 ): NextAuthOptions {
   return {
     adapter: PrismaAdapter(req, res),
