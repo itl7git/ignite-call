22070a14e2c308fe3acbd639656858b51d45cf34
Italo Leal <itlcoorporativo@gmail.com>
2023-10-20 18:29:11 -0300

    Calendário e disponibilidade - Bloqueando horários no passado

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -66,7 +66,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month') + 1,
+          month: String(currentDate.get('month') + 1).padStart(2, '0'),
         },
       })
 
