4a303a3fe61efe9c7072678918c04707dbde6cd4
Italo Leal <itlcoorporativo@gmail.com>
2023-10-24 21:20:32 -0300

    Deploy: Atualiza o next.config para corrigir o bug do phosphor-icons em produção(Too many open files)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/react/{{member}}',
+    },
+  },
 }
 
 module.exports = nextConfig
